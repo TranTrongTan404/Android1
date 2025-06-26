5dcc912096f3c8846666d4d89bf6f118e99687ad
Trần Trọng Tấn <trantrongtan404@gmail.com>
2025-06-26 11:28:04 +0700

    Final state to overwrite remote

--- app/src/main/java/com/example/project_tanapp/HomeActivity.java
+++ app/src/main/java/com/example/project_tanapp/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.project_tanapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,33 @@ public class HomeActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button GoToProfile =findViewById(R.id.btnUser);
+        GoToProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(it);
+            }
+        });
+
+        Button GoToCart =findViewById(R.id.btnCart);
+        GoToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(getApplicationContext(), CartActivity.class);
+                startActivity(it);
+            }
+        });
+
+        Button GoToD1 =findViewById(R.id.btnD1);
+        GoToD1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ot = new Intent(getApplicationContext(), DetailProduct1Activity.class);
+                startActivity(ot);
+            }
         });
     }
 }
